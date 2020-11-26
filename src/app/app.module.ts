953e7383a1f173bd810bc76eeeab23b94f64aab4
abhishekgiram <abhishek.giram11@gmail.com>
2020-11-26 17:19:11 +0530

    navigation is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, NavigationBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
